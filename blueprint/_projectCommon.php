b89d928aac484697121b0fd01fa76b7ecb43fc5c
cleau <cleau>
2010-03-30 13:29:25 +0000

    + roll out the changes back in small steps

--- blueprint/_projectCommon.php
+++ blueprint/_projectCommon.php
@@ -49,7 +49,7 @@
 	# $Menu->addMenuItem("Developers", "/project/developers", "_self");
 	
 	# To define additional CSS or other pre-body headers
-	$App->AddExtraHtmlHeader('<link media="screen" rel="stylesheet" type="text/css" href="/blueprint/resources/style.css"/>');
+	$App->AddExtraHtmlHeader('<link media="screen" rel="stylesheet" type="text/css" href="/blueprint/resources/local.css"/>');
 	
 	# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
 	$App->Promotion = TRUE;
