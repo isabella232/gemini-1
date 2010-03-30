799f375ec036f69fbbee6b194ff80b7702b24ab1
cleau <cleau>
2010-03-30 12:37:02 +0000

    + roll out the changes back in small steps

--- blueprint/index.php
+++ blueprint/index.php
@@ -10,15 +10,24 @@
  *    
  *******************************************************************************/
 
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+	
+	$App 	= new App();	
+	$Nav	= new Nav();	
+	$Menu 	= new Menu();		
+	
+	include($App->getProjectCommon());
+	
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/blueprint/resources/style.css"/>');
 	
 	$localVersion = false;
 	
 	# Define these here, or in _projectCommon.php for site-wide values
 	#$pageKeywords	= "eclipse, osgi, blueprint, spring, ioc, dynamic, services, rfc-124";
-	$pageAuthor		= "Costin Leau";
-	$pageTitle 		= "Eclipse Gemini Blueprint - Incubation";
+	#$pageAuthor		= "Costin Leau";
+	$pageTitle 		= "Eclipse Gemini Blueprint - Home";
 	
 	
 	// 	# Paste your HTML content between the EOHTML markers!
