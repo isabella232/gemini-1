40aa84f274a078c798ef99b1b5f27da555eb0b8b
mkeith <mkeith>
2010-08-13 19:23:23 +0000

    *** empty log message ***

--- dbaccess/download/index.php
+++ dbaccess/download/index.php
@@ -23,7 +23,11 @@
 	$pageTitle 		= "Gemini DBAccess - Downloads";
 	$pageKeywords	= "Eclipse, EclipseRT, OSGi, DBAccess, Downloads";
 	$pageAuthor		= "Mike Keith";
-		
+
+	$milestoneM1 = "1.0.0.M1-incubation";
+
+	$latestMilestone = "1.0.0.M1-incubation";
+
 	ob_start();
 ?>
 	
@@ -33,9 +37,13 @@
 			All downloads are provided under the terms and conditions of the 
 			<a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.
 		</p>
+        <h2>Latest Milestone of Gemini DBAccess: <?=$latestMilestone ?></h2>
+		<br/>
 		<p>
-		    No downloads are currently available.
+			<strong>Gemini DBAccess</strong> -  
+			<a href="http://www.eclipse.org/downloads/download.php?file=/rt/gemini/dbaccess/r1.0/milestones/gemini-dbaccess-<?=$milestoneM1 ?>.zip" target="_self">Download</a>
 		</p>
+
 	</div>
 
 	<!-- remove the entire <div> tag to omit the right column!  -->
