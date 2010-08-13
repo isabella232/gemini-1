703e707708594289fceed5c38736a94c0ab39d19
mkeith <mkeith>
2010-08-13 19:31:25 +0000

    *** empty log message ***

--- dbaccess/_projectCommon.php
+++ dbaccess/_projectCommon.php
@@ -31,7 +31,7 @@
 	$Nav->addCustomNav("Documentation", 	"/gemini/dbaccess/documentation",	"_self", 3);
 	$Nav->addCustomNav("Support", 			"/gemini/dbaccess/support", 		"_self", 3);
 	$Nav->addCustomNav("Getting Involved", 	"/gemini/dbaccess/contribute",		"_self", 3);
-	$Nav->addCustomNav("About DB Access",	"/gemini/dbaccess/about",    		"_self", 3);
+	$Nav->addCustomNav("About DBAccess",	"/gemini/dbaccess/about",    		"_self", 3);
 
 	# Define keywords, author and title here, or in each PHP page specifically
 	# $pageKeywords	= "eclipse, OSGi, gemini";
