acb082a0d7348d8f134f9cd6656fc978f6a879d6
lkirchev <lkirchev>
2011-12-06 06:48:45 +0000

    Update Gemini Management documentation page. See bug 365690.

--- management/documentation/index.php
+++ management/documentation/index.php
@@ -17,11 +17,12 @@
  * Contributors: 
  *               Mike Keith
  *               Peter Peshev
+ *               Lazar Kirchev
  *    
  *******************************************************************************/
 
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Gemini DBAccess - Documentation";
+	$pageTitle 		= "Gemini Management - Documentation";
 	$pageKeywords	= "Eclipse, EclipseRT, Gemini, OSGi, Management, Documentation";
 	$pageAuthor		= "Peter Peshev";
 		
@@ -35,16 +36,35 @@
         This specification may be downloaded
 			<a href="http://www.osgi.org/Download/Release4V42">here</a>.		
 		</p>
+		
+		<h3>Extensions to the Specification</h3>
+		<p>
+		Gemini Management extends the JMX Specification Version 1.0 interface <code>BundleStateMBean</code> with a method <code>TabularData listBundles(int mask)</code>.
+		The original interface has the same method, but without arguments. It returns a <code>TabularData</code> object, containing all details for all bundles. The new method
+		allows to specify (through the mask) what details to be present in the result. For example, in this way only the ids of all bundles could be retrieved, without any
+		other details. In the case of a system with a large number of bundles this could considerably improve the performance since much less data is transferred through the 
+		network connection.
+		</p>
+		<p>
+		Gemini Management also extends the <code>ServiceStateMBean</code> with methods for introspection of a single service:
+		<ul>
+			<li><code>CompositeData getService(long serviceId)</code> - get service by its id</li>
+			<li><code>CompositeData getProperty(long serviceId, String key)</code> - get the value of a property of a particular service</li>
+			<li><code>TabularData listServices(String clazz, String filter)</code> - get all services implementing an interface and satisfying a filter</li>
+			<li><code>TabularData listServices(String clazz, String filter, String... serviceTypeItems)</code> - get all services implementing an interface, satisfying a filter and specifying particular properties</li>
+			<li><code>long[] getServiceIds()</code> - get a list of all service ids</li>
+		</ul>
+		</p>
+		<p>
+		Both extensions are included in the next version 4.3 of the Enterprise Specification.
+		</p>
+		
 		<h4>Wiki Documentation</h4>
 		Please refer to the Gemini Management wiki for further documentation
 			<ul>
 				<li><a href="http://wiki.eclipse.org/Gemini/Management">Gemini Management</a></li>
 				<li><a href="http://wiki.eclipse.org/Gemini/Management/GettingStarted">Getting Started</a></li>
 			</ul>
-		<p>
-		The documentation has not been finished yet for this project. If you would like to volunteer to write, or assist in 
-		writing them, please send an email to the <a href="https://dev.eclipse.org/mailman/listinfo/gemini-dev">developer mailing list</a> or post to the <a href="http://www.eclipse.org/forums/index.php?t=thread&frm_id=153">forum</a>.
-		</p>
 	</div>
 
 <?
