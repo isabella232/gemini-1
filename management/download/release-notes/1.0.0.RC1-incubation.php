9124fb372dc6f73dd7af0b7f9ac9947b972903c2
cfrost <cfrost>
2012-01-06 15:34:46 +0000

    Updates in preparation for the release

--- management/download/release-notes/1.0.0.RC1-incubation.php
+++ management/download/release-notes/1.0.0.RC1-incubation.php
@@ -32,18 +32,18 @@
 	
 	<p>
 		This is the release candidate of Gemini Management, no more features will be added. The documentation 
-		will be improved before the release but assuming there are no bugs found this will be formal release 
-		of 1.0.0 of Gemini Management. You can see all the bugs that have been completed for this release below. 
+		will be improved before the release but assuming there are no bugs found this will be the first formal 
+		release of Gemini Management. You can see all the bugs that have been completed for this release below. 
 		The big changes for this milestone are as follows.
 	</p>
 	<p>
 		Some improved testing and minor performance improvements along with more Java doc and better code layout.
 	</p>
 	<p>
-		Implementation of new methods for the upcoming changes in the next version of the Enterprise spec, these changes are 
-		outlined in RFC 169, a public draft of this is avaliable <a href="http://www.osgi.org/download/osgi-early-draft-2011-09.pdf">
-		here</a>. Please note these aditional methods do not affect the spec 1.0 functionality and are likely to change as the 
-		new spec hasn't been finalized.
+		Implementation of new methods for the upcoming changes in the OSGi Enterprise JMX spec, these changes are 
+		outlined in RFC 169, a public draft of this is available <a href="http://www.osgi.org/download/osgi-early-draft-2011-09.pdf">
+		here</a>. Please note these additional methods do not affect the 1.0 spec functionality and are likely to 
+		change as the new spec hasn't been finalised yet.
 	</p>
 	<ul>
 		<li>New BundleStateMBean methods to allow a single bundle and individual manifest headers to be inspected.</li>
@@ -51,10 +51,10 @@
 		<li>New ServiceStateMBean methods to allow a single service to be inspected.</li>
 		<li>New ServiceStateMBean methods to allow the fields in a listing of services to be filtered.</li>
 		<li>New FrameworkMBean methods for refreshing a bundle and getting some wiring information.</li>
-		<li>Initial implementation of the WiringStateMBean which will replace the deprecated PacakgeAdminMBean.</li>
+		<li>Initial implementation of the WiringStateMBean which will replace the deprecated PacakgeStateMBean.</li>
 	</ul>
 	<p>
-		Bug list is available <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;classification=RT;product=Gemini.Management;target_milestone=1.0.0.M02-incubation" target="_self">here</a>.
+		Full bug list is available <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;classification=RT;product=Gemini.Management;target_milestone=1.0.0.M02-incubation" target="_self">here</a>.
 	</p>
 
 </div>
