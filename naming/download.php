c00742d42721a389d29c717f486f835e6ceee84a
vgeorgieva <vgeorgieva>
2012-03-04 18:18:32 +0000

    In addition to previous change

--- naming/download.php
+++ naming/download.php
@@ -18,13 +18,13 @@
 <h2>$pageTitle</h2>
 <p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
-<h2>Latest Milestone of Gemini Naming</h2>
+<h2>Latest Milestone</h2>
  <p>
      <strong>1.0.0.M02</strong> - 
      <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.M02.zip">Download</a>
  </p>
 
-<h3>Previous Milestones of Gemini Naming</h3>
+<h3>Previous Milestones</h3>
 <p>
      <strong>1.0.0.M01-incubation</strong> - 
      <a href="http://www.eclipse.org/downloads/download.php?file=/naming/milestones/gemini-naming-1.0.0.M01-incubation/gemini-naming-1.0.0.M01-incubation.zip">Download</a>
