64b9e23b2a474c3eb5cc7f667585f8b1d5e4b92f
mkeith <mkeith>
2012-03-19 20:59:35 +0000

    *** empty log message ***

--- gemini/download.php
+++ gemini/download.php
@@ -62,18 +62,6 @@
 
 </div>
 
-/*****
- <div id="rightcolumn">
-	<div class="sideitem">
-		<h6>Incubation</h6>
-		<div id="incubation" align="center">
-	   		<a href="/projects/what-is-incubation.php">
-	   			<img align="center" src="/images/egg-incubation.png" border="0" alt="Incubation" />
-	   		</a>
-	   	</div>
-	</div>	
- </div>
-*****/
 <?
 	$html = ob_get_clean();
 
