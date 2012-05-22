d3f8f1bed4e39fac405e2166c2d4ccb1f4c8deb5
vgeorgieva <vgeorgieva>
2012-05-22 17:58:45 +0000

    Added information for Gemini Naming 1.0.0.RELEASE

--- naming/download.php
+++ naming/download.php
@@ -15,39 +15,44 @@
 	#$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/gemini/naming/style.css"/>');
 
 	$html  = <<<EOHTML
-	<script type="text/javascript">
-            window.addEvent('domready', function() {
-                    new Accordion('h4.toggle', 'div.accordion', {initialDisplayFx: false});
-            });
-	</script>
-
 
 <div id="midcolumn">
 	<h2>$pageTitle</h2>
 	<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
+	<h2>Gemini Naming Downloads</h2>
+	
+    <h4>1.0.0.RELEASE</h4>
+	<div>
+		<ul>
+ 		<li><strong>Gemini Naming</strong> - <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/gemini-naming-1.0.0.RELEASE.zip">Download</a></li>
+		<li><strong>Gemini Naming p2 repository</strong> - <a href='http://download.eclipse.org/gemini/updates/naming/1.0.0/' target='_self'>http://download.eclipse.org/gemini/updates/naming/1.0.0/</a></li>
+		<li><strong>Bug list is available</strong> - <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT;query_format=advanced;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;target_milestone=1.0.0.M01;target_milestone=1.0.0.M02;target_milestone=1.0.0.RC1;target_milestone=1.0.0.RELEASE;product=Gemini.Naming" target="_self">here</a>.</li>
+		</ul>
+	</div>
+
 	<h2>Development Milestones</h2>
     
-    <h4 class='toggle'>1.0.0.RC1</h4>
-	<div class='accordion'>
+    <h4>1.0.0.RC1</h4>
+	<div>
 		<ul>
- 		<li><strong>Gemini Naming</strong> -  <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.RC1.zip">Download</a></li>
+ 		<li><strong>Gemini Naming</strong> - <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.RC1.zip">Download</a></li>
 		<li><strong>Bug list is available</strong> - <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT;query_format=advanced;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;target_milestone=1.0.0.RC1;product=Gemini.Naming" target="_self">here</a>.</li>
 		</ul>
 	</div>
 		
-    <h4 class='toggle'>1.0.0.M02</h4>
-	<div class='accordion'>
+    <h4>1.0.0.M02</h4>
+	<div>
 		<ul>
- 		<li><strong>Gemini Naming</strong> -  <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.M02.zip">Download</a></li>
+ 		<li><strong>Gemini Naming</strong> - <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.M02.zip">Download</a></li>
 		<li><strong>Bug list is available</strong> - <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT;query_format=advanced;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;target_milestone=1.0.0.M02;product=Gemini.Naming" target="_self">here</a>.</li>
 		</ul>
 	</div>
 
-    <h4 class='toggle'>1.0.0.M01-incubation</h4>
-	<div class='accordion'>
+    <h4>1.0.0.M01-incubation</h4>
+	<div>
 		<ul>
- 		<li><strong>Gemini Naming</strong> -  <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.M01-incubation.zip">Download</a></li>
+ 		<li><strong>Gemini Naming</strong> - <a href="http://www.eclipse.org/downloads/download.php?file=/gemini/naming/r1.0/milestones/gemini-naming-1.0.0.M01-incubation.zip">Download</a></li>
 		<li><strong>Bug list is available</strong> - <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT;query_format=advanced;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;target_milestone=1.0.0.M01;product=Gemini.Naming" target="_self">here</a>.</li>
 		</ul>
 	</div>
